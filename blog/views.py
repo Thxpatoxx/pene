39898dcd5df7e63e1fedabdbd1ac27a1ba3f2e25
patto <p.oyarzuno@alumnos.duoc.cl>
2019-05-14 13:48:17 -0400

    Templates modificados para mostrar post desde base de datos.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Implementodep
 
 def post_list(request):
-    return render(request,'blog/post_list.html',{})
+    Implementodeps = Implementodep.objects.order_by('folio')
+    return render(request, 'blog/post_list.html', {'Implementodeps': Implementodeps})
